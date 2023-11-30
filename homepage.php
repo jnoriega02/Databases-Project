70c15d94da4fe6f476566246dcec97b811cf070f
Diana Alvarez <z1919487@turing.cs.niu.edu>
2023-11-29 19:48:43 -0600

    commi

--- homepage.php
+++ homepage.php
@@ -140,7 +140,7 @@
 
     			<div class="product-details">
         		<h2>Cozy Sweater: $29.99</h2>
-        		<p>In Stock: 30</p>
+        		<p>In Stock: </p>
         		<label for="quantity-product-1">Quantity:</label>
 			<select id="quantity-product-1" class="quantity-dropdown">
 
@@ -161,7 +161,7 @@
 
 			<div class="product-details">
 			<h2>Cozy Hat: $9.99</h2>
-			<p>In Stock: 15</p>
+			<p>In Stock: </p>
 			<label for="quantity-product-1">Quantity:</label>
 			<select id="quantity-product-1" class="quantity-dropdown">
 
@@ -178,7 +178,7 @@
 			<img src="https://img.freepik.com/premium-photo/female-jeans-mom-fit-isolated
 			-white-background-jeans-with-high-waist-casual-style_125869-2331.jpg" alt="Jeans">
 			<h2>Jeans: $49.99</h2>
-			<p>In Stock: 20</p>
+			<p>In Stock: </p>
 			<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
@@ -186,7 +186,7 @@
 			<img src="https://www.converse.com/dw/image/v2/BCZC_PRD/on/demandware.static
 			/-/Sites-cnv-master-catalog/default/dw34a014b4/images/a_107/162050C_A_107X1.jpg?sw=964" alt="Converse">
 			<h2>Converse: $59.99</h2>
-			<p>In Stock: 17</p>
+			<p>In Stock: </p>
 			<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
@@ -201,7 +201,7 @@
 		<div class="product">
 			<img src="https://media.kohlsimg.com/is/image/kohls/5850895_Black_Fabric?wid=805&hei=805&op_sharpen=1" alt="Boots">
 			<h2>Boots: $40.99</h2>
-			<p>In Stock: 32</p>
+			<p>In Stock: </p>
 			<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
@@ -209,109 +209,106 @@
 			<img src="https://jiffyshirts.imgix.net/f9447d7b5ba227.png?ixlib=rb-0.3.5&auto=format&fit=
 			fill&fill=solid&trim-color=FFFFFF&trim=color&trim-tol=8&w=307&h=480&q=80&dpr=2" alt="Sweatshirt">
 			<h2>Sweatshirt: $25.99 </h2>
-			<p>In Stock: 50</p>
+			<p>In Stock: </p>
 			<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
 		<div class="product">
 			<img src="https://m.media-amazon.com/images/I/71iHxbJXPBL._AC_SX679_.jpg" alt="Beanie">
 			<h2>Beanie: $12.99</h2>
-			<p>In Stock: 15</p>
+			<p>In Stock: </p>
 			<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 9">
-			<h2>Product 9</h2>
-			<p>In Stock: 0</p>
+			<img src="https://m.media-amazon.com/images/I/61ByLPJnfyL.jpg" alt="Mushroom Sweater">
+			<h2>Mushroom Sweater: $25.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 10">
-			<h2>Product 10</h2>
-			<p>In Stock: 0</p>
+			<img src="https://www.converse.com/dw/image/v2/BCZC_PRD/on/demandware.static/-/Sites-cnv-master
+			-catalog/default/dw1fc8d527/images/a_08/560845C_A_08X1.jpg?sw=406" alt="Platform Converse">
+			<h2>Platform Converse: $79.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 11">
-			<h2>Product 11</h2>
-			<p>In Stock: 0</p>
+			<img src="https://m.media-amazon.com/images/I/71kK6NxG-rL._AC_SY741_.jpg" alt="Skirt">
+			<h2>Skirt: $14.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 12">
-			<h2>Product 12</h2>
+			<img src="https://img1.shopcider.com/product/1662119464000-Sdfhkn.jpg?x-oss
+			-process=image/resize,w_350,m_lfit/quality,Q_80/interlace,1" alt="Corduroy Pants">
+			<h2>Corduroy Pants: $39.99</h2>
 			<p>In Stock: 0</p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 		
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 13">
-			<h2>Product 13</h2>
-			<p>In Stock: 0</p>
+			<img src="https://static.halsbrook.com/media/catalog/product/majestic-blackmodalturtlenecktop-1413386443-product.jpg"
+			 alt="Black Turtleneck">
+			<h2>Black Turtleneck: $14.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 	    	</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 14">
-			<h2>Product 14</h2>
-			<p>In Stock: 0</p>
+			<img src="https://www.alexmill.com/cdn/shop/files/WW206-3171-WALNUT-25382_1440x1920
+			_crop_center.jpg?v=1691089211" alt="Brown Turtleneck">
+			<h2>Brown Turtleneck: $19.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 	    	</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 15">
-			<h2>Product 15</h2>
-			<p>In Stock: 0</p>
+			<img src="https://www.legendarywhitetails.com/on/demandware.static/-/Sites-master
+			-us/default/dwd621b640/images/6624_RGRP_MAIN_FRONT.jpg" alt="Flannel Jacket">
+			<h2>Flannel Jacket: $39.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 	    	</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 16">
-			<h2>Product 16</h2>
-			<p>In Stock: 0</p>
+			<img src="https://www.marinelayer.com/cdn/shop/files/M_H1_Archive_Andes_Sherpa
+			_Pullover_Mountain_Scene_4230103092265-Final-Web_700x.jpg?v=1697148787" alt="Mountain Jacket">
+			<h2>Mountain Jacket: $29.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 		</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 17">
-			<h2>Product 17</h2>
-			<p>In Stock: 0</p>
+			<img src="https://www.legendarywhitetails.com/on/demandware.static/-/Sites-master
+			-us/default/dw5faed3bc/images/6170_MVBU_MAIN_FRONT.jpg" alt="Green Flannel">
+			<h2>Green Flannel: $19.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 	    	</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 18">
-			<h2>Product 18</h2>
-			<p>In Stock: 0</p>
+			<img src="https://cdnimg.emmiol.com/E/202211/img_original-VCK0192TO-210206123.jpg" alt="Winter Sweater">
+			<h2>Winter Sweater: $49.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 	    	</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 19">
-			<h2>Product 19</h2>
-			<p>In Stock: 0</p>
+			<img src="https://cdni.llbean.net/is/image/wim/206990_2089_41?hei=804&wid=
+			700&resMode=sharp2&defaultImage=llbprod/206990_2089_41" alt="Cozy Socks">
+			<h2>Cozy Socks: $9.99</h2>
+			<p>In Stock: </p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 	    	</div>
 
 		<div class="product">
-			<img src="https://st.depositphotos.com/2934765/53192/v/450/depositphotos_531920820
-			-stock-illustration-photo-available-vector-icon-default.jpg" alt="Product 20">
-			<h2>Product 20</h2>
+			<img src="https://www.bostongeneralstore.com/cdn/shop/products/mens
+			-wool-fair-isle-socks-207953_1200x.jpg?v=1696872294" alt="Winter Socks">
+			<h2>Winter Socks: $11.99</h2>
 			<p>In Stock: 0</p>
                 	<button class="add-to-cart-button">Add to Cart</button>
 		</div>
